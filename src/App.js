42207c2f5bb17ba033b6e56c64eb4b4a09d50298
yaaHosein <layali.14.peshawar@hotmail.com>
2021-06-22 13:52:35 +0430

    creating flowers document

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import {Flowers} from "./Flowers";
+function App() {
+    return ( 
+    <div className = "App">
+        <Flowers/>
+        </div>
+    );
+}
+
+export default App;
