bad4124786241a699574e75fc77f645ef854ab7c
yaaHosein <layali.14.peshawar@hotmail.com>
2021-06-29 12:52:46 +0430

    removing the extra img tag

--- src/Flowers.js
+++ src/Flowers.js
@@ -2,7 +2,6 @@ export function Flowers(props) {
     return <>
         <h3>{props.title}</h3>
         <p>{props.description}</p>
-        <img src={props.imageSource} />
-        <img alt={props.imageDescription} />
+        <img src={props.imageSource} alt={props.imageDescription} />
     </>
 };
