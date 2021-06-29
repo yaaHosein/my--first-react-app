7e630115082649eceb6780deb6b70174c2e4e1dc
yaaHosein <layali.14.peshawar@hotmail.com>
2021-06-29 12:43:43 +0430

    using propes to add some details abt flowers

--- src/Flowers.js
+++ src/Flowers.js
@@ -1,19 +1,8 @@
-export function Flowers() {
+export function Flowers(props) {
     return <>
-        <h3>SILAS MARNER</h3>
-        <p>An unfussy rose of soft mid pink, the petals are pale on the reverse and fade at the edges creating a gentle quality.</p>;
-        <img src="C:\Users\Dell\Desktop\my--first-react-app\public\images\SILAS MARNER.jpg" alt="SILAS MARNER" />;
-        <h3>GABRIEL OAK</h3>
-        <p>A magnificent variety, bearing large, many petalled rosette blooms. They are a striking shade of deep pink, the outer petals of each bloom paling slightly over time. The beauty of the blooms is enhanced by a wonderful, strong fruity fragrance. </p>;
-
-        <img src="C:\Users\Dell\Desktop\my--first-react-app\public\images\GABRIEL OAK.jpg" alt="GABRIEL OAK" />;
-        <h3>HARLOW CARR</h3>
-        <p>Bears flowers of the most perfect formation – shallow cups of the purest mid pink. They flower very freely from early summer until well into the autumn and have a strong, pure Old Rose fragrance. </p>;
-        <img src="C:\Users\Dell\Desktop\my--first-react-app\public\images\HARLOW CARR.jpg" alt="HARLOW CARR.jpg" />;
+        <h3>{props.title}</h3>
+        <p>{props.description}</p>
+        <img src={props.imageSource} />
+        <img alt={props.imageDescription} />
     </>
-
-
-
-
-
 };
