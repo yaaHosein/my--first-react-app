6081dbe48500278131e633091c928ad215869fd8
yaaHosein <layali.14.peshawar@hotmail.com>
2021-06-29 16:51:41 +0430

    placing imageDescription by imageName

--- src/Flowers.js
+++ src/Flowers.js
@@ -2,6 +2,6 @@ export function Flowers(props) {
     return <>
         <h3>{props.title}</h3>
         <p>{props.description}</p>
-        <img src={props.imageSource} alt={props.imageDescription} />
+        <img src={props.imageSource} alt={props.imageName} />
     </>
 };
