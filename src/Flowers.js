350e612f923ebe6ca0e4b84b34eed6952380fd0d
yaaHosein <layali.14.peshawar@hotmail.com>
2021-07-10 12:45:19 +0430

    making collapse functionallity to be implemented in Flowers componenr 2

--- src/Flowers.js
+++ src/Flowers.js
@@ -1,3 +1,29 @@
+import { useState } from "react/cjs/react.production.min"
+export const Collapsable = () => {
+    let [isShow, setIsShow] = useState(true);
+    let buttonText = "";
+    if (isShow) {
+        buttonText = "Hide"
+    } else {
+        buttonText = "Show"
+    }
+    return
+    <div>
+        <button
+            onClick={() => {
+                setIsShow(!isShow);
+            }}
+        >
+            {buttonText}
+        </button>
+        {isShow && (
+            <p>
+                "A magnificent variety, bearing large, many petalled rosette blooms. They are a striking shade of deep pink, the outer petals of each bloom paling slightly over time. The beauty of the blooms is enhanced by a wonderful, strong fruity fragrance."
+            </p>
+        )};F
+
+    </div >
+};
 export function Flowers(props) {
     return <>
         <h3>{props.title}</h3>
